7c562b961645a2cbe8371c6c3c21f9c3f3d465a7
Zachary-Graeber <130615647+Zachary-Graeber@users.noreply.github.com>
2023-06-05 15:19:41 -0700

    a

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,39 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private int moveSpeed;
-    public int dashSpeed = 10;
-    [SerializeField] private GameObject playerObject;
-    private PlayerController playerController;
-    private Vector2 movementDirection;
-    private float timeSinceLastDash = 0f;
-    private float dashCooldown = 2f;
-
-    [SerializeField] private Animator anim;
-
-    private void Update()
-    {
-        timeSinceLastDash += Time.deltaTime;
-
-        //Getting speed based off of playercontroller because speed can change
-        playerController = playerObject.GetComponent<PlayerController>();
-
-        moveSpeed = playerController.Speed; 
-
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        playerObject.transform.Translate(movementDirection * Time.deltaTime * moveSpeed);
-
-        
-        if (Input.GetKeyDown(KeyCode.Space) && timeSinceLastDash >= dashCooldown)
-        {
-            Dash();
+    // Start is called before the first frame update
+    [SerializeField] float playerSpeed;
+    [SerializeField] float diagonalMultiplier;
+    [SerializeField] float speed;
+    void move(int vert,int horiz,int speedMultiplier){
+        if(vert!=0 && horiz!=0){
+            vert*=diagonalMultiplier;
+            horiz*=diagonalMultiplier;
         }
-    }
-
-    private void Dash()
-    {
-        timeSinceLastDash = 0;
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        playerObject.transform.Translate(movementDirection * Time.deltaTime * dashSpeed);
+        this.position+=Vector3(horiz*playerSpeed,vert*playerSpeed,0);
     }
 }
