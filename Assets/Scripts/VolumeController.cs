f3cb496f71de1f605ff6e5b617c9710fa9796805
Benlee1000 <benny01lee@gmail.com>
2023-06-08 20:52:13 -0700

    Added music, sfx, and sliders to adjust them in both menus

---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+/*
+ * Sets the music or sfx level based on options sliders
+ * Learned how to do this from https://www.youtube.com/watch?v=xNHSGMKtlv4
+ */
+public class VolumeController : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetMusicLevel (float sliderValue)
+    {
+        // Adjusts for decibel levels being in a log scale
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+    }
+    public void SetSFXLevel(float sliderValue)
+    {
+        // Adjusts for decibel levels being in a log scale
+        mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
